0ff7f642ffdc06153204f108579f1032704989d6
Hector Sanchez Pajares <hector.sanchezpajares@here.com>
2017-05-08 14:06:49 +0200

    Augmented images

--- clone.py
+++ clone.py
@@ -5,6 +5,7 @@ import numpy as np
 lines = []
 with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
+    next(reader) # Skip the header
     for line in reader:
         lines.append(line)
 images = []
@@ -12,24 +13,41 @@ measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = './data/IMG' + filename
+    current_path = './data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
-    measurement = float(lines[3])
+    measurement = float(line[3])
     measurements.append(measurement)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_images.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement * -1.0)
 
-X_train = np.array(images)
-y_train = np.array(mesaurements)
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 
-from keras.model import Sequential
-from keras.layers import Flatten, Dense
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6, (5, 5), activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, (5, 5), activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 
 model.save('model.h5')
+
+exit()
